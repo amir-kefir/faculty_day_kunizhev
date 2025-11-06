628a1195f0725eb16cad35621cc86335f156f795
amir-kefir <aakunizhev@icloud.com>
2025-11-06 10:44:34 +0300

    my first task (couldn't check the code cuz I cant run it)

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Main.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Main.kt
@@ -1,5 +1,21 @@
 package ru.tbank.education.school.lesson1
 
 fun main() {
-    println("Hello world!")
+    val start = "amir"
+    val mid = '-'
+    val end = "kefir"
+
+    val str = """
+        My git username starts with $amir,
+        has "$mid" in the middle
+        and ends with $end
+    """.trimIndent()
+    println(str)
+
+    var num = 0
+    num += 1
+    println(num)
+
+    var n: Int? = Null
+    println(n)
 }
