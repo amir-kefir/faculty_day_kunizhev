f89f50bb45be75aecb1fe97ac9f2ffae007ccff8
amir-kefir <aakunizhev@icloud.com>
2025-11-06 11:17:41 +0300

    second task

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/EvenNumbers.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/EvenNumbers.kt
@@ -1,8 +1,19 @@
 package ru.tbank.education.school.lesson1
 
-/**
- * Сумма четных чисел.
- */
 fun sumEvenNumbers(numbers: Array<Int>): Int {
-    TODO()
+    var count = 0
+    for (i in numbers) {
+        if (i % 2 == 0) {
+            count += i
+        }
+    println(count)
+    }
+}
+
+fun sumEvenNumbers2(numbers: Array<Int>): Int =
+    numbers.filter{ it % 2 == 0}.sum()
+
+fun main() {
+    var array = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    println(sumEvenNumbers2(array))
 }
